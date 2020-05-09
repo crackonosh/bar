375a2c91b13b9e9f2d9366aad5d198232c3248c4
Ja <lukashais@disroot.org>
2020-05-09 10:47:02 +0200

    Temporarily drop spotify support. Scripted seems somehow bugged

--- script/music.sh
+++ script/music.sh
@@ -10,11 +10,6 @@ case $cmusRunning in
       song=$(/usr/local/bin/cmus-remote -Q | grep "tag title" | sed s/"tag title"/""/g |  sed '1s/^.//')
       artist=$(/usr/local/bin/cmus-remote -Q | grep "tag artist" | sed s/"tag artist"/""/g)
       echo $artist - $song
-    else
-      echo $(osascript -e 'tell application "System Events"set processList to (name of every process)end tellif (processList contains "Spotify") is true thentell application "Spotify"if player state is playing thenset artistName to artist of current trackset trackName to name of current trackreturn artistName & " - " & trackNameelsereturn ""end ifend tellend if')
     fi
-    ;;
-  *)
-     echo $(osascript -e 'tell application "System Events"set processList to (name of every process)end tellif (processList contains "Spotify") is true thentell application "Spotify"if player state is playing thenset artistName to artist of current trackset trackName to name of current trackreturn artistName & " - " & trackNameelsereturn ""end ifend tellend if')
     ;;
 esac
