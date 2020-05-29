1dc6d0c2be320dc3e09cae9b0de6908a8d7b38dd
Ja <lukashais@disroot.org>
2020-05-29 23:06:24 +0200

    spotify finally works again with applescript

--- script/music.sh
+++ script/music.sh
@@ -10,6 +10,11 @@ case $cmusRunning in
       song=$(/usr/local/bin/cmus-remote -Q | grep "tag title" | sed s/"tag title"/""/g |  sed '1s/^.//')
       artist=$(/usr/local/bin/cmus-remote -Q | grep "tag artist" | sed s/"tag artist"/""/g)
       echo $artist - $song
+    else
+      echo $(osascript -e 'tell application "System Events"set processList to (name of every process)end tellif (processList contains "Spotify") is true thentell application "Spotify"if player state is playing thenset artistName to artist of current trackset trackName to name of current trackreturn artistName & " - " & trackNameelsereturn ""end ifend tellend if')
     fi
+    ;;
+  *)
+    echo $(osascript -e 'tell application "System Events"set processList to (name of every process)end tellif (processList contains "Spotify") is true thentell application "Spotify"if player state is playing thenset artistName to artist of current trackset trackName to name of current trackreturn artistName & " - " & trackNameelsereturn ""end ifend tellend if')
     ;;
 esac
