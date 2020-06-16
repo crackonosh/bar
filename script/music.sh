3f6a8af03148fa5a0b12ac7627dfc6daf64519a2
Ja <lukashais@disroot.org>
2020-06-16 11:11:14 +0200

    Update music script when spotify is not active || not playing

--- script/music.sh
+++ script/music.sh
@@ -4,17 +4,33 @@ cmusRunning=$(echo "$(ps aux | grep -o "cmus$")")
 
 case $cmusRunning in
   "cmus")
-    playStatus=$(echo "$(/usr/local/bin/cmus-remote -Q | grep status | awk '{print $2}')")
-    if [ $playStatus == "playing" ];
+    cmusPlayStatus=$(echo "$(/usr/local/bin/cmus-remote -Q | grep status | awk '{print $2}')")
+    if [ $cmusPlayStatus == "playing" ];
     then
       song=$(/usr/local/bin/cmus-remote -Q | grep "tag title" | sed s/"tag title"/""/g |  sed '1s/^.//')
       artist=$(/usr/local/bin/cmus-remote -Q | grep "tag artist" | sed s/"tag artist"/""/g)
       echo $artist - $song
     else
-      echo $(osascript -e 'tell application "Spotify" to get (artist of current track as string) & " - " & (name of current track as string)')
+      spPlayStatus=$(echo "$(ps aux | grep Spotify | wc -l)")
+      if [ $spPlayStatus -g 1 ]
+      then
+        tmp=$(osascript -e 'tell application "Spotify" to get player state')
+        if [ $tmp = 'playing' ]
+        then
+          echo $(osascript -e 'tell application "Spotify" to get (artist of current track as string) & " - " & (name of current track as string)')
+        fi
+      fi
     fi
     ;;
   *)
-    echo $(osascript -e 'tell application "Spotify" to get (artist of current track as string) & " - " & (name of current track as string)')
+    spPlayStatus=$(echo "$(ps aux | grep Spotify | wc -l)")
+    if [ $spPlayStatus -ge 2 ]
+    then
+      tmp=$(osascript -e 'tell application "Spotify" to get player state')
+      if [ $tmp = 'playing' ]
+      then
+        echo $(osascript -e 'tell application "Spotify" to get (artist of current track as string) & " - " & (name of current track as string)')
+      fi
+    fi
     ;;
 esac
